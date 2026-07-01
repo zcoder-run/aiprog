1381213f487d9cc258647bb36fd186ab251432df
Jeremy Chone <jeremy.chone@gmail.com>
2026-07-01 14:55:09 -0700

    . c02-custom - minor rename

--- examples/c02-custom.rs
+++ examples/c02-custom.rs
@@ -1,9 +1,12 @@
+use std::fs;
+
 use aiprog::{
 	AipFromLua, AipIntoLua, AipOutput, AipParams, AipRegistry, Error, HandlerResult, LuaExt, ScriptEngine, aip_handler,
 	register_handler,
 };
 use schemars::JsonSchema;
 use serde::{Deserialize, Serialize};
+use simple_fs::{SPath, ensure_file_dir};
 use value_ext::JsonValueExt as _;
 
 // region:    --- Types
@@ -25,21 +28,21 @@ impl AipParams for GreetingParams {}
 
 /// The greeting
 #[derive(Debug, Deserialize, Serialize, JsonSchema)]
-struct GreetingResult(String);
+struct GreetingOutput(String);
 
-impl AipIntoLua for GreetingResult {
+impl AipIntoLua for GreetingOutput {
 	fn into_lua(self, lua: &mlua::Lua) -> aiprog::Result<mlua::Value> {
 		let s = lua.create_string(&self.0).map_err(|e| Error::custom(format!("{}", e)))?;
 		Ok(mlua::Value::String(s))
 	}
 }
 
-impl AipOutput for GreetingResult {}
+impl AipOutput for GreetingOutput {}
 
 /// Custom greeting handler.
 #[aip_handler]
-fn custom_greetings(params: GreetingParams) -> HandlerResult<GreetingResult> {
-	Ok(GreetingResult(format!("Hello {}", params.name)))
+fn custom_greetings(params: GreetingParams) -> HandlerResult<GreetingOutput> {
+	Ok(GreetingOutput(format!("Hello {}", params.name)))
 }
 
 // endregion: --- Handler
@@ -61,11 +64,12 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 
 	println!("{}", result.x_pretty()?);
 
-	// -- print doc
+	// -- Save doc
 
-	// let doc = script_engine.generate_doc()?;
-
-	// println!("\n=== Doc:\n{doc}");
+	let doc = script_engine.generate_doc()?;
+	let out_file = SPath::new("examples/.out/c02-doc.md");
+	ensure_file_dir(&out_file)?;
+	fs::write(out_file, doc)?;
 
 	Ok(())
 }
